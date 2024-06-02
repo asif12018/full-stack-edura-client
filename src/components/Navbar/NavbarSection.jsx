f339c816380fb6a14ef7f00c49d91d20b3c03ba3
asif12018 <asif.request@gmail.com>
2024-06-02 22:47:25 +0600

    logo added

--- src/components/Navbar/NavbarSection.jsx
+++ src/components/Navbar/NavbarSection.jsx
@@ -1,12 +1,12 @@
 
 import { Navbar } from "flowbite-react";
-
+import { FaGoogleScholar } from "react-icons/fa6";
 const NavbarSection = () => {
     return (
         <div className="">
             <Navbar fluid className="py-6 !bg-[#14452f]">
                 <Navbar.Brand href="https://flowbite-react.com">
-                    
+                <FaGoogleScholar className="text-white text-4xl mr-4"/>
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Edura</span>
                 </Navbar.Brand>
                 <Navbar.Toggle />
