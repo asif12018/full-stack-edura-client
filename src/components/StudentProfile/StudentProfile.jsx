1e96dcd197a2051d8e0d38a3581b6cec8f377502
asif12018 <asif.request@gmail.com>
2024-06-12 19:07:24 +0600

    bugs fixed

--- src/components/StudentProfile/StudentProfile.jsx
+++ src/components/StudentProfile/StudentProfile.jsx
@@ -199,7 +199,7 @@ const StudentProfile = () => {
                             <h4 className="text-xl text-gray-900 font-bold">Purchase</h4>
                             <p className="mt-2 text-gray-700">View your overall purchase  from here</p>
 
-
+{/* 
                             <ResponsiveContainer width="100%" height={300}>
                                 <AreaChart width={730} height={250} data={chartData}
                                     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
@@ -220,7 +220,23 @@ const StudentProfile = () => {
                                     <Area type="monotone" dataKey="paid" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
                                     <Area type="monotone" dataKey="courseTitle" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
                                 </AreaChart>
-                            </ResponsiveContainer>
+                            </ResponsiveContainer> */}
+
+<ResponsiveContainer width="100%" height={400}>
+                                    <AreaChart data={chartData}>
+                                        <defs>
+                                            <linearGradient id="colorPaid" x1="0" y1="0" x2="0" y2="1">
+                                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                            </linearGradient>
+                                        </defs>
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <Tooltip />
+                                        <Area type="monotone" dataKey="paid" stroke="#8884d8" fillOpacity={1} fill="url(#colorPaid)" />
+                                    </AreaChart>
+                                </ResponsiveContainer>
 
 
                             
